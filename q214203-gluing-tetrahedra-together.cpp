9288fb1764d635c79e7a96a53ea1894f8af6cc7b
David Ellsworth <david@worms2d.info>
2025-11-22 23:08:31 -0800

    Fix the symmetry array sizes

--- q214203-gluing-tetrahedra-together.cpp
+++ q214203-gluing-tetrahedra-together.cpp
@@ -883,8 +883,8 @@ void enumerate(
 #if defined(PRINT_SYMMETRY_TOTALS) || defined(PRINT_POLYTETS_WITH_SYMMETRY)
     SymmetryRoot newRotatedPolytetList    [MAXIMUM_TETCOUNT * 3];
     SymmetryRoot newRotatedPolytetSym3List[MAXIMUM_TETCOUNT / 3];
-    int8_t symmetryList [MAXIMUM_TETCOUNT * 3 / 2 + 1];
-    int8_t symmetry3List[MAXIMUM_TETCOUNT / 3     + 1];
+    int8_t symmetryList [MAXIMUM_TETCOUNT * 3 + 1];
+    int8_t symmetry3List[MAXIMUM_TETCOUNT / 3 + 1];
 #endif
 
 #ifdef MULTITHREADING
