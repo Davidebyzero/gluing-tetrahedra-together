Davidebyzero/gluing-tetrahedra-together

commit 1ef322089569bc662695007ac57a8a6b3d71ce99

Precalculate overlap cache
Since this is done pivoting back and forth to add new cells on either end, USE_GMP should not be needed until twice the MAXIMUM_TETCOUNT value as before.

q214203-gluing-tetrahedra-together.cpp
@@ -1113,39 +1113,16 @@ void enumerate(
 #ifndef DISABLE_OVERLAP_CHECKING
                     // Check for overlap between this newly attached tetrahedron and the existing ones,
                     // and defer this until after the deduplication, to save a lot of time
-                    overlap.setA(newTet);
-                    // Set up the "skipOverlapCheck" flags to skip overlap checking up to a depth of 5
                     newTet.tagSkipOverlapCheck(polytet, minOverlapDepth);
                     for (int tetCheckIntersectionI=0; tetCheckIntersectionI<polytet.size(); tetCheckIntersectionI++)
                     {
                         if (polytet[tetCheckIntersectionI].compressedPath == UINT64_MAX)
                             continue; // skip this check for speed (it'll always be false anyway)
                         CompressedSubpolytet compressedPath = (polytet[tetCheckIntersectionI].compressedPath - 1) / 3;
-                        if (compressedPath >= minUnseenCompressedSubpolytet)
+                        if (compressedPath >= 1 && overlapCache[compressedPath - 1])
                         {
-                            overlap.setB(polytet[tetCheckIntersectionI]);
-                            if (auto overlapResult = overlap(maximalTouchingSqrDistance))
-                            {
-#   ifdef PRINT_POLYTETS_EDGE_CASES
-                                if (overlapResult < 0)
-                                {
-                                    boost::mutex::scoped_lock lock(printPolytetMutex);
-                                    printPolytet(runningLeastPolytet[0].value, polytet);
-                                }
-#   endif
-                                overlapCache[                      compressedPath  - 1] = true;
-                                overlapCache[reflectCompressedPath(compressedPath) - 1] = true;
-                                foundOverlaps = true;
-                                goto skipDueToOverlap;
-                            }
-                        }
-                        else
-                        {
-                            if (overlapCache[compressedPath - 1])
-                            {
-                                foundOverlaps = true;
-                                goto skipDueToOverlap;
-                            }
+                            foundOverlaps = true;
+                            goto skipDueToOverlap;
                         }
                     }
 #endif // DISABLE_OVERLAP_CHECKING
@@ -1360,6 +1337,73 @@ int main(int argc, char *argv[])
     {
         pool = malloc(poolSize = MEMORY_POOL_INITIAL_SIZE);
         if (!pool) quitMemory();
+    }
+
+    // Populate overlapCache
+    {
+        for (int i=4; i<MAXIMUM_TETCOUNT; i+=2)
+            mul_start_3(start, maximalTouchingSqrDistance, minUnseenCompressedSubpolytet);
+        Polytet polytet;
+        polytet.init(start);
+        int tetCount = 2, topCount = 1, bottomCount = 1;
+        int8_t faceNumStack[MAXIMUM_TETCOUNT];
+        int stackPos = 0;
+        faceNumStack[0] = 0;
+        TetrahedronOverlap overlap;
+
+        for (;;)
+        {
+            if (tetCount == MAXIMUM_TETCOUNT || faceNumStack[stackPos] == 3)
+            {
+                if (tetCount > 5)
+                {
+                    Tet  &newEndTet = polytet[tetCount - 1];
+                    Tet &prevEndTet = polytet[tetCount - 2];
+                    newEndTet.tagSkipOverlapCheck(polytet, 5);
+                    CompressedSubpolytet compressedPath = (prevEndTet.compressedPath - 1) / 3;
+                    overlap.setA( newEndTet);
+                    overlap.setB(prevEndTet);
+                    if (overlap(maximalTouchingSqrDistance))
+                    {
+                        overlapCache[compressedPath - 1] = true;
+#if 0
+                        int i;
+                        /*for (i=0; i<stackPos; i++) printf("%d", faceNumStack[i]);
+                        printf(" = ");*/
+                        for (i = stackPos - 1; i>=0; i-=2) printf("%d", faceNumStack[i]);
+                        i = stackPos & 1;
+                        for (; i<stackPos; i+=2) printf("%d", faceNumStack[i]);
+                        printf(", ");
+                        int64_t blah = compressedPath;
+                        while (blah)
+                        {
+                            blah--;
+                            printf("%u", blah % 3);
+                            blah /= 3;
+                        }
+                        printf(", 0x%llX = %d\n", compressedPath - 1, overlapCache[compressedPath - 1]);
+#endif
+                    }
+                }
+                if (--stackPos < 0)
+                    break;
+                if (bottomCount > topCount) bottomCount--;
+                else                           topCount--;
+                tetCount--;
+                polytet[tetCount - 2].faceAttached[faceNumStack[stackPos]] = -1;
+                polytet[tetCount - 2].isLeaf = true;
+                faceNumStack[stackPos]++;
+            }
+            else
+            {
+                bool attachToTop = bottomCount > topCount;
+                int iAttachTo = tetCount - 2;
+                if (attachToTop)    topCount++;
+                else             bottomCount++;
+                attachNewTet(polytet, tetCount++, iAttachTo, faceNumStack[stackPos]);
+                faceNumStack[++stackPos] = 0;
+            }
+        }
     }
 
 #ifdef MULTITHREADING

config-sample.h
@@ -1,6 +1,6 @@
 // Copy this file to "config.h" and edit as needed
 
-#define USE_GMP
+//#define USE_GMP
 #define MEMORY_POOL_INITIAL_SIZE (64uLL * 1024)  // in bytes; if the goal is to use more than half of available RAM, this must be preallocated at full expected size
 #define MEMORY_POOL_GROW_RATIO 1/16  // what proportion of the memory size to grow it by when more space is needed
 #define HASH_TABLE_RATIO 6

commit 1026b200331d87f592e88b6c6073427918f72e4e

Fix PRINT_POLYTETS_EDGE_CASES
This was broken in 1ef322089569bc662695007ac57a8a6b3d71ce99.
It's still far from ideal, but at least it works now.

q214203-gluing-tetrahedra-together.cpp
@@ -1347,7 +1347,29 @@ int main(int argc, char *argv[])
                         overlap.setB(prevEndTet);
                         foundOverlap = overlap(maximalTouchingSqrDistance);
                         if (foundOverlap)
+                        {
+#ifdef PRINT_POLYTETS_EDGE_CASES
+                            if (foundOverlap < 0)
+                            {
+                                SymmetryRoot runningLeastPolytet[2];
+                                bool isChiral;
+    #if defined(PRINT_SYMMETRY_TOTALS) || defined(PRINT_POLYTETS_WITH_SYMMETRY)
+                                SymmetryType symmetryType;
+                                int symmetry;
+    #endif
+                                polytet.setSize(tetCount);
+                                CompressedPolytet newRotatedPolytet(polytet);
+                                canonicalizePolytet(polytet, tetCount, runningLeastPolytet, newRotatedPolytet, isChiral
+    #if defined(PRINT_SYMMETRY_TOTALS) || defined(PRINT_POLYTETS_WITH_SYMMETRY)
+                                    , symmetryType
+                                    , symmetry
+    #endif
+                                    );
+                                printPolytet(runningLeastPolytet[0].value, polytet);
+                            }
+#endif
                             overlapCache[compressedPath - 1] = 1;
+                        }
                         else
                             overlapCache[compressedPath - 1] = -1;
                     }