Davidebyzero/gluing-tetrahedra-together

commit 1466874788e7d98afff872e2a07dd83e3c238711

Fix coordinate precision for PRINT_POLYTETS or PRINT_POLYTETS_WITH_SYMMETRY
It was broken in 618a2b074e1d4dcf46d4bc3c4da46e70b8f0b8e8.

q214203-gluing-tetrahedra-together.cpp
@@ -1288,6 +1288,9 @@ int main(int argc, char *argv[])
                 if (resumeFile) fclose(resumeFile);
                 resumeFile = f;
                 tetCount = i;
+#if defined(PRINT_POLYTETS) || defined(PRINT_POLYTETS_WITH_SYMMETRY)
+                mul_start_3(start, maximalTouchingSqrDistance, minUnseenCompressedSubpolytet);
+#endif
             }
             else
                 break;
@@ -1324,8 +1327,15 @@ int main(int argc, char *argv[])
 
     // Precompute overlapCache
     {
+#if defined(PRINT_POLYTETS) || defined(PRINT_POLYTETS_WITH_SYMMETRY)
+        auto start2 = start;
+        auto maximalTouchingSqrDistance2 = maximalTouchingSqrDistance;
+#else
+        auto &start2 = start;
+        auto &maximalTouchingSqrDistance2 = maximalTouchingSqrDistance;
+#endif
         for (int i=5; i<MAXIMUM_TETCOUNT; i+=2)
-            mul_start_3(start, maximalTouchingSqrDistance);
+            mul_start_3(start2, maximalTouchingSqrDistance2);
         Polytet polytet;
         polytet.init<true>(start);
         int tetCount = 2, topCount = 1, bottomCount = 1;
@@ -1352,7 +1362,7 @@ int main(int argc, char *argv[])
                         auto reflectedCompressedPath = reflectCompressedPath(compressedPath);
                         overlap.setA( newEndTet);
                         overlap.setB(prevEndTet);
-                        foundOverlap = overlap(maximalTouchingSqrDistance);
+                        foundOverlap = overlap(maximalTouchingSqrDistance2);
                         if (foundOverlap)
                         {
 #ifdef PRINT_POLYTETS_EDGE_CASES
@@ -1605,6 +1615,9 @@ int main(int argc, char *argv[])
 #endif
 
         resumedFromFile = false;
+#if defined(PRINT_POLYTETS) || defined(PRINT_POLYTETS_WITH_SYMMETRY)
+        mul_start_3(start, maximalTouchingSqrDistance, minUnseenCompressedSubpolytet);
+#endif
     }
 
 errorQuit:

commit 3ffc9aeabe98b284b3858d64b71519ef6a54cdeb

Fix compiling with PRINT_POLYTETS / PRINT_POLYTETS_WITH_SYMMETRY
This was broken in 1466874788e7d98afff872e2a07dd83e3c238711.

q214203-gluing-tetrahedra-together.cpp
@@ -60,6 +60,12 @@ typedef uint64_t CompressedSubpolytet; // compressed in branchless format, in bi
 #ifdef USE_GMP
 class Tetrahedron
 {
+    void init()
+    {
+        for (int p=0; p<5; p++)
+            for (int d=0; d<3; d++)
+                mpz_init(t[p][d]);
+    }
 public:
     mpz_t t[5][3];
     Tetrahedron &operator=(const Tetrahedron &_t)
@@ -71,9 +77,12 @@ public:
     }
     Tetrahedron()
     {
-        for (int p=0; p<5; p++)
-            for (int d=0; d<3; d++)
-                mpz_init(t[p][d]);
+        init();
+    }
+    Tetrahedron(const Tetrahedron &_t)
+    {
+        init();
+        *this = _t;
     }
     ~Tetrahedron()
     {
@@ -1275,7 +1284,7 @@ int main(int argc, char *argv[])
                 resumeFile = f;
                 tetCount = i;
 #if defined(PRINT_POLYTETS) || defined(PRINT_POLYTETS_WITH_SYMMETRY)
-                mul_start_3(start, maximalTouchingSqrDistance, minUnseenCompressedSubpolytet);
+                mul_start_3(start, maximalTouchingSqrDistance);
 #endif
             }
             else
@@ -1320,8 +1329,14 @@ int main(int argc, char *argv[])
     // Precompute overlapCache
     {
 #if defined(PRINT_POLYTETS) || defined(PRINT_POLYTETS_WITH_SYMMETRY)
-        auto start2 = start;
+        Tetrahedron start2(start);
+    #ifdef USE_GMP
+        mpz_t maximalTouchingSqrDistance2;
+        mpz_init(maximalTouchingSqrDistance2);
+        mpz_set (maximalTouchingSqrDistance2, maximalTouchingSqrDistance);
+    #else
         auto maximalTouchingSqrDistance2 = maximalTouchingSqrDistance;
+    #endif
 #else
         auto &start2 = start;
         auto &maximalTouchingSqrDistance2 = maximalTouchingSqrDistance;
@@ -1329,7 +1344,7 @@ int main(int argc, char *argv[])
         for (int i=5; i<MAXIMUM_TETCOUNT; i+=2)
             mul_start_3(start2, maximalTouchingSqrDistance2);
         Polytet polytet;
-        polytet.init<true>(start);
+        polytet.init<true>(start2);
         int tetCount = 2, topCount = 1, bottomCount = 1;
         int8_t faceNumStack[MAXIMUM_TETCOUNT];
         int stackPos = 0;
@@ -1422,6 +1437,9 @@ int main(int argc, char *argv[])
                 faceNumStack[++stackPos] = -1;
             }
         }
+#if defined(USE_GMP) && (defined(PRINT_POLYTETS) || defined(PRINT_POLYTETS_WITH_SYMMETRY))
+        mpz_clear(maximalTouchingSqrDistance2);
+#endif
     }
 #ifdef WRITE_TO_FILES
     writeFile(OVERLAP_BITMAP_FILENAME, (uint8_t*)overlapCache, overlapCacheSize);
@@ -1603,7 +1621,7 @@ int main(int argc, char *argv[])
 
         resumedFromFile = false;
 #if defined(PRINT_POLYTETS) || defined(PRINT_POLYTETS_WITH_SYMMETRY)
-        mul_start_3(start, maximalTouchingSqrDistance, minUnseenCompressedSubpolytet);
+        mul_start_3(start, maximalTouchingSqrDistance);
 #endif
     }
 