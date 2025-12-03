Davidebyzero/gluing-tetrahedra-together

commit b5db66fda12f5274fb1f44c4641a7c8add59f339

Leave the zero-initialization of nextIndexTable to the caller, all at once, instead of enumerate() one at a time
This not only eliminates the slowdown of ccba62d91a0f62638ff4167cf453cb631893f24c, but allows its table-separation to result in a slight speed-up.

q214203-gluing-tetrahedra-together.cpp
@@ -1226,10 +1226,10 @@ void enumerate(
                         polytetChiralCount += isChiral;
                         memcpy(entry, &runningLeastPolytet[0].value, newPolytetsCompressedSize);
 #ifdef MULTITHREADING
-                        nextHashCollisionTable[newPolytetCountFetched] = 0; // pointer to next hash collision
+                        //nextHashCollisionTable[newPolytetCountFetched] = 0; // unnecessary thanks to the "memset(nextIndexTable, ...)"
                         *index =               newPolytetCountFetched + 1;
 #else
-                        nextHashCollisionTable[newPolytetCount       ] = 0; // pointer to next hash collision
+                        //nextHashCollisionTable[newPolytetCount       ] = 0; // unnecessary thanks to the "memset(nextIndexTable, ...)"
                         *index =             ++newPolytetCount;
 #endif
                     }
@@ -1364,7 +1364,7 @@ int main(int argc, char *argv[])
             poolSize = size;
             if (poolSize < MEMORY_POOL_INITIAL_SIZE)
                 poolSize = MEMORY_POOL_INITIAL_SIZE;
-            pool = malloc(poolSize);
+            pool = calloc(poolSize, 1);
             if (!pool)
             {
                 fclose(resumeFile);
@@ -1387,7 +1387,7 @@ int main(int argc, char *argv[])
     if (!pool)
 #endif
     {
-        pool = malloc(poolSize = MEMORY_POOL_INITIAL_SIZE);
+        pool = calloc(poolSize = MEMORY_POOL_INITIAL_SIZE, 1); // use calloc() so that nextIndexTable[] is zero-initialized
         if (!pool) quitMemory();
     }
 
@@ -1683,7 +1683,8 @@ int main(int argc, char *argv[])
         memoryUsage = (uint8_t*)nextHashCollisionTable + newPolytetCount * polytetTableElementSize - (uint8_t*)pool;
 
         polytetCount = newPolytetCount;
-        memmove(basePolytetTable, polytetTable, newPolytetsCompressedSize * newPolytetCount);
+        memmove(basePolytetTable, polytetTable, newPolytetCount * newPolytetsCompressedSize);
+        memset (nextHashCollisionTable, 0,      newPolytetCount * sizeof(HashIndex)); // initialize this here so that it doesn't have to be individually done for each new entry
 
 #ifdef WRITE_TO_FILES
         writeFile(getCompressedPolytetFilename(tetCount), basePolytetTable, polytetCount * newPolytetsCompressedSize);

commit 11e81b9dfa7292989e7d41c93f41772eb1f7de65

Fix end of polytet table being overwritten with zeroes after copy
Bug introduced in b5db66fda12f5274fb1f44c4641a7c8add59f339

Related changes:
* Initialize only as much of memory pool (hash table, etc.) as is necessary, in light of the memory having been allocated with calloc()

q214203-gluing-tetrahedra-together.cpp
@@ -1331,6 +1331,9 @@ int main(int argc, char *argv[])
     size_t polytetCount = 1;
     size_t memoryUsage = 0;
 
+    struct {void *start, *end;} zeroesNeeded[2];
+    zeroesNeeded[0].start = NULL;
+
     int tetCount=1;
     bool resumedFromFile = false;
 #ifdef MULTITHREADING
@@ -1639,7 +1642,20 @@ int main(int argc, char *argv[])
         const int polytetTableElementSize = newPolytetsCompressedSize + sizeof(HashIndex);
         polytetTableMaxSize = ((uint8_t*)pool + poolSize - (uint8_t*)nextHashCollisionTable) / polytetTableElementSize;
         polytetTable = nextHashCollisionTable + polytetTableMaxSize;
-        memset(hashTable, 0, hashTableSize * sizeof(HashIndex));
+
+        // Initialize nextHashCollisionTable with zeroes here so that it doesn't have to be individually done for each new entry
+        // (which would reduce the efficieny of CPU caching). Also might as well combine the hashTable initialization into this.
+        if (zeroesNeeded[0].start)
+        {
+            for (int i=0; i<_countof(zeroesNeeded); i++)
+            {
+                if (zeroesNeeded[i].start < hashTable)
+                    zeroesNeeded[i].start = hashTable;
+                auto diff = (intptr_t)((uint8_t*)zeroesNeeded[i].end - (uint8_t*)zeroesNeeded[i].start);
+                if (diff > 0)
+                    memset(zeroesNeeded[i].start, 0, diff);
+            }
+        }
         size_t newPolytetCount = 0;
 
         size_t nextWorkAssignment = 0;
@@ -1713,12 +1729,18 @@ int main(int argc, char *argv[])
             (polytetTable, newPolytetCount, newPolytetsCompressedSize, sorter, (void*)(size_t)newPolytetsCompressedSize);
 #endif
 
+        zeroesNeeded[0].start = basePolytetTable;
+        zeroesNeeded[0].end   = nextHashCollisionTable + newPolytetCount;
+
         polytetCount = newPolytetCount;
-        memmove(basePolytetTable, polytetTable, polytetCount * newPolytetsCompressedSize);
-        memset (nextHashCollisionTable, 0,      polytetCount * sizeof(HashIndex)); // initialize this here so that it doesn't have to be individually done for each new entry
+        auto polytetByteCount = polytetCount * newPolytetsCompressedSize;
+        memmove(basePolytetTable, polytetTable, polytetByteCount);
+
+        zeroesNeeded[1].start =           polytetTable;
+        zeroesNeeded[1].end   = (uint8_t*)polytetTable + polytetByteCount;
 
 #ifdef WRITE_TO_FILES
-        writeFile(getCompressedPolytetFilename(tetCount), basePolytetTable, polytetCount * newPolytetsCompressedSize);
+        writeFile(getCompressedPolytetFilename(tetCount), basePolytetTable, polytetByteCount);
 #endif
 
         resumedFromFile = false;